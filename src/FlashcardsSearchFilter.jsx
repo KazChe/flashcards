2004f5e8fc4b2cdc21ea6cc77594f22b2dc6c247
kazche <kaz.che@gmail.com>
2023-12-30 14:28:47 -0800

    reactjs virtual dom benefits and drawbacks

--- src/FlashcardsSearchFilter.jsx
+++ src/FlashcardsSearchFilter.jsx
@@ -1,3 +1,12 @@
+import { useState } from "react";
+
 export default function FlashcardSearchFilter() {
-    
+    const [category, setCategory] = useState(setCategory)
+    const [tag, setTag] = useState(setTag)
+
+    return (
+        <div>
+            
+        </div>
+    )
 }
